8c40a76fd7f154edc5eea3ade0ffdb4568fcfb61
Kevin Friedman <kevinfriedman@nypl.org>
2017-07-09 13:39:37 -0400

    Updates for 1.0.28

--- src/Formatter/NyplLogFormatter.php
+++ src/Formatter/NyplLogFormatter.php
@@ -5,11 +5,34 @@ use Monolog\Formatter\JsonFormatter;
 
 class NyplLogFormatter extends JsonFormatter
 {
-    protected function translateLevel($level = 0)
+    protected function translageLevelToInteger($level = '')
+    {
+        switch ($level) {
+            case 'DEBUG':
+                return 7;
+            case 'INFO':
+                return 6;
+            case 'NOTICE':
+                return 5;
+            case 'WARNING':
+                return 4;
+            case 'ERROR':
+                return 3;
+            case 'CRITICAL':
+                return 2;
+            case 'ALERT':
+                return 1;
+            case 'EMERGENCY':
+                return 0;
+        }
+    }
+
+
+    protected function translateMonologLevelToString($level = 0)
     {
         switch ($level) {
             case 100:
-                return 'DEBUG';
+                return 'DEBUGS';
             case 200:
                 return 'INFO';
             case 250:
@@ -27,17 +50,28 @@ class NyplLogFormatter extends JsonFormatter
         }
     }
 
+    /**
+     * @param array $record
+     *
+     * @return string
+     */
     public function format(array $record)
     {
-        $record['level'] = $this->translateLevel($record['level']);
-        
+        $record['level'] = $this->translateMonologLevelToString($record['level']);
+
+        $record['levelCode'] = $this->translageLevelToInteger($record['level']);
+
         $record['datetime'] = date('c');
 
-        unset($record['level_name']);
-        unset($record['channel']);
+        unset($record['level_name'], $record['channel']);
 
-        if (!$record['extra']) unset($record['extra']);
-        if (!$record['context']) unset($record['context']);
+        if (!$record['extra']) {
+            unset($record['extra']);
+        }
+
+        if (!$record['context']) {
+            unset($record['context']);
+        }
 
         $record = parent::format($record);
 

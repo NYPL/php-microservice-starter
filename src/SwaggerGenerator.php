227d26226c32ffe7802080f373e14898f235fae2
John Berube <johnberube@nypl.org>
2024-09-19 16:59:38 -0400

    Adds back options to json response.

--- src/SwaggerGenerator.php
+++ src/SwaggerGenerator.php
@@ -9,25 +9,18 @@ class SwaggerGenerator
 {
     public static function generate(array $directory, Response $response)
     {
-        //ErrorHandler::setIgnoreError(true);
+        ErrorHandler::setIgnoreError(true);
 
         $generator = new Generator(new DefaultLogger());
-
         $openapi = $generator->scan($directory, ['exclude' => ['tests'], 'pattern' => '*.php']);
-
-//        $openapi->host = Config::get('SWAGGER_HOST');
-//
-//        $openapi->basePath = '/api';
-//
-//        $openapi->schemes = [
-//            Config::get('SWAGGER_SCHEME')
-//        ];
-
-        $json = $openapi->toJson();
-
-
+        $docs = json_decode($openapi->toJson());
+        $docs->host = Config::get('SWAGGER_HOST');
+        $docs->basePath = '/api';
+        $docs->schemes = [
+            Config::get('SWAGGER_SCHEME')
+        ];
+        $json = json_encode($docs);
         $streamBody = fopen('data://text/plain,' . $json,'r');
-
         return $response->withHeader('Content-Type', 'application/json')
                 ->withHeader('Access-Control-Allow-Origin', '*')
                 ->withHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
@@ -37,27 +30,6 @@ class SwaggerGenerator
                     'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'
                 )
                 ->withBody(new \GuzzleHttp\Psr7\Stream($streamBody));
-
-
-//
-//        $finder = \Symfony\Component\Finder\Finder::create()->files()->name('*.php')->in($directory);
-//
-//        $openapi = (new \OpenApi\Generator())->generate([$directory, $finder]);
-//
-//        $json = $openapi->toJson();
-//
-//
-//        $streamBody = fopen('data://text/plain,' . $json,'r');
-//
-//        return $response->withHeader('Content-Type', 'application/json')
-//                ->withHeader('Access-Control-Allow-Origin', '*')
-//                ->withHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-//                ->withHeader('Access-Control-Allow-Credentials', 'true')
-//                ->withHeader(
-//                    'Access-Control-Allow-Headers',
-//                    'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'
-//                )
-//                ->withBody(new \GuzzleHttp\Psr7\Stream($streamBody));
 
     }
 }

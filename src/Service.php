1b28a165449d7cdfc30a8610d4192bbcf2d8f831
John Berube <johnberube@nypl.org>
2024-10-17 17:06:02 +0200

    Removes setupDefaultRoutes() function.

--- src/Service.php
+++ src/Service.php
@@ -31,41 +31,5 @@ class Service extends App
         $app->addBodyParsingMiddleware();
 
         parent::__construct($app->getResponseFactory(), $container);
-
-        $this->setupDefaultRoutes();
-    }
-
-    protected function setupDefaultRoutes()
-    {
-        $this->add(function (Request $request, Response $response, callable $next) {
-            $response = $next($request, $response);
-            return $response
-                ->withHeader(
-                    "Access-Control-Allow-Headers",
-                    "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token"
-                )
-                ->withHeader(
-                    "Access-Control-Allow-Methods",
-                    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-                )
-                ->withHeader(
-                    "Access-Control-Allow-Origin",
-                    "*"
-                )
-                ->withHeader(
-                    "Access-Control-Allow-Credentials",
-                    "true"
-                )
-                ->withHeader('X-NYPL-Original-Request', $request->getUri())
-                ->withHeader('X-NYPL-Response-Date', date('c'));
-        });
-
-        $this->options("[/{params:.*}]", function (Request $request, Response $response) {
-            return $response
-                ->withHeader(
-                    "Cache-Control",
-                    "public, max-age=" . self::CACHE_SECONDS_OPTIONS_REQUEST
-                );
-        });
     }
 }

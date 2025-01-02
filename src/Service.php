76d2fc23e65841de81e0e5bbc00db8741839590f
John Berube <johnberube@nypl.org>
2025-01-02 15:42:22 -0500

    Cleans up Service.php.

--- src/Service.php
+++ src/Service.php
@@ -3,12 +3,9 @@ namespace NYPL\Starter;
 
 use Aura\Di\Container;
 use Aura\Di\ContainerBuilder;
-use Aura\Di\Injection\InjectionFactory;
 use Slim\App;
 use Slim\Factory\AppFactory;
-use GuzzleHttp\Psr7\Request;
-use GuzzleHttp\Psr7\Response;
-use NYPL\Starter\DefaultMiddleware;
+
 class Service extends App
 {
     const CACHE_SECONDS_OPTIONS_REQUEST = 600;
